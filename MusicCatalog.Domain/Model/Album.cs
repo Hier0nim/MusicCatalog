282b1896a81647a20ed3ccce0a4f1003bf03c095
Hieronim <hieronimdaniel@gmail.com>
2023-04-16 18:37:24 +0200

    Add project files.

---
+++ MusicCatalog.Domain/Model/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCatalog.Domain.Model
+{
+    internal class Album
+    {
+        private int Id { get; set; }
+        private string Title { get; set; }
+        private  string Artist{ get; set; }
+
+        public virtual ICollection<Track> Tracks{ get;}
+    }
+}
---
+++ MusicCatalog.Domain/Model/Track.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCatalog.Domain.Model
+{
+    internal class Track
+    {
+        private int Id { get; set; }
+        private string Title { get; set; }
+        private string Artist { get; set; }
+        public virtual Album Album { get; set; }
+    }
+}
